4b5cb24449f190822e89da2163244c73d8b0354a
Tanesha L. Washington <tanesha.dixon@gmail.com>
2026-05-22 01:12:30 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,24 +1,17 @@
--- Question 1:
--- Calculate the average deliver time per carrier, ordered from slowest to fastest. 
+Scenario:
 
-SELECT 
-	ca.carrier_name, 
-	AVG(s.delivery_time_days) AS avg_delivery
-FROM Shipment s
-JOIN Carrier ca 
-	ON s.carrier_id = ca.carrier_id
-GROUP BY ca.carrier_name
-ORDER BY avg_delivery DESC
+-- You’ve been brought in as a junior data analyst for a regional bank. Your task is to analyze customer transaction data, identify trends in account activity, and help flag potential communication issues for client outreach. You’ll work with three tables: Customer, Account, and Transaction.
 
 
--- Question 2: 
--- Generate a list of distinct delivery statuses used across all shipments.
 
-SELECT DISTINCT delivery_status FROM Shipment
-
--- Question 3:
--- Create a list of customers showing their full names and label missing emails as 'email needed'.
+-- Question 1: Identify the customer with the highest total transaction amount across all their accounts. 
 
 
--- Question 4:
--- List all shipments with customer and carrier names, including shipments for customers who don't have an email address on file. 
+-- Question 2: Find the total transaction amount per account, and filter for accounts where the total is greater than $1,000.
+
+-- Question 3: Create a report showing each customer's full name. 
+
+
+-- Question 4: Provide a list of distinct transaction types recorded in the system. 
+
+-- Question 5: Show a list of all customers with their account types and balances, even if the customer doesn't have an email address on file. 
